1e709620679a5130927f5cc87d012e2b06ec6022
Łukasz Dembiński <dembol@devesion.com>
2014-11-10 18:42:07 +0100

    some test fixes

--- src/test/java/com/devesion/obd/command/protocol/AbstractSetStateCommandTest.java
+++ src/test/java/com/devesion/obd/command/protocol/AbstractSetStateCommandTest.java
@@ -1,15 +1,15 @@
 package com.devesion.obd.command.protocol;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
-public abstract class AbstractSetStateCommandTest {
+abstract class AbstractSetStateCommandTest {
 
 	private ProtocolCommand sut;
 
 	@Test
-	public void constructorShouldSetSpacesOff() throws Exception {
+	protected void constructorShouldSetStateOff() throws Exception {
 		// given
 		String expectedOperands = getOperandPrefix() + "0";
 
@@ -22,7 +22,7 @@ public abstract class AbstractSetStateCommandTest {
 	}
 
 	@Test
-	public void constructorShouldSetEchoOn() throws Exception {
+	protected void constructorShouldSetStateOn() throws Exception {
 		// given
 		String expectedOperands = getOperandPrefix() + "1";
 
--- src/test/java/com/devesion/obd/command/protocol/SetLineFeedCommandTest.java
+++ src/test/java/com/devesion/obd/command/protocol/SetLineFeedCommandTest.java
@@ -1,6 +1,18 @@
 package com.devesion.obd.command.protocol;
 
+import org.testng.annotations.Test;
+
 public class SetLineFeedCommandTest extends AbstractSetStateCommandTest {
+
+	@Test
+	public void constructorShouldSetStateOff() throws Exception {
+		super.constructorShouldSetStateOff();
+	}
+
+	@Test
+	public void constructorShouldSetStateOn() throws Exception {
+		super.constructorShouldSetStateOn();
+	}
 
 	@Override
 	protected String getOperandPrefix() {
